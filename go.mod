c9d4b17d773144e80833577055a461b76d54da7c
JohnathanFL <jfl747076@gmail.com>
2020-02-18 17:56:39 -0600

    Basic Server implemented. About to design the protocol itself

--- go.mod
+++ go.mod
@@ -1,3 +1,22 @@
 module github.com/JohnathanFL/ligujo
 
 go 1.13
+
+require (
+	github.com/gogo/protobuf v1.3.1 // indirect
+	github.com/golang/protobuf v1.3.3 // indirect
+	github.com/hashicorp/golang-lru v0.5.4 // indirect
+	github.com/neelance/parallel v0.0.0-20160708114440-4de9ce63d14c // indirect
+	github.com/opentracing/basictracer-go v1.0.0 // indirect
+	github.com/opentracing/opentracing-go v1.1.0 // indirect
+	github.com/prometheus/client_golang v1.4.1 // indirect
+	github.com/prometheus/procfs v0.0.10 // indirect
+	github.com/slimsag/godocmd v0.0.0-20161025000126-a1005ad29fe3 // indirect
+	github.com/sourcegraph/ctxvfs v0.0.0-20180418081416-2b65f1b1ea81 // indirect
+	github.com/sourcegraph/go-langserver v2.0.0+incompatible // indirect
+	github.com/sourcegraph/jsonrpc2 v0.0.0-20191222043438-96c4efab7ee2 // indirect
+	golang.org/x/mod v0.2.0 // indirect
+	golang.org/x/net v0.0.0-20200202094626-16171245cfb2 // indirect
+	golang.org/x/sys v0.0.0-20200217220822-9197077df867 // indirect
+	golang.org/x/tools v0.0.0-20200218205902-f8e42dc47720 // indirect
+)
--- ligujo.go
+++ ligujo.go
@@ -1,11 +1,41 @@
 package main
 
 import (
-  "fmt"
-//  "io/ioutil"
-//  "net/http"
+	"fmt"
+	"net/http"
+	"time"
 )
 
+type RequestServer struct{}
+
+func (r RequestServer) ServeHTTP(resp http.ResponseWriter, req *http.Request) {
+    // We'll allow PUT/POST as synonyms since I can't find a good reason to restrict.
+    if req.Method == "PUT" || req.Method == "POST" {
+        HandleUpdate(resp, req)
+    } else if req.Method == "GET" {
+        HandleRequest(resp, req)
+    }
+}
+
+// An interpreter has connected to us to give us new information
+func HandleUpdate(res http.ResponseWriter, req *http.Request) {
+    fmt.Println("Handling update...")
+}
+
+// An editor has connected to us to ask for information
+func HandleRequest(res http.ResponseWriter, req *http.Request) {
+    fmt.Println("Handling request...")
+}
+
 func main() {
-  fmt.Println("Saluton, mondo!")
+	fmt.Println("Saluton, mondo!")
+	server := &http.Server{
+		Addr:           ":8080",
+		Handler:        &RequestServer{},
+		ReadTimeout:    20 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+
+	server.ListenAndServe()
 }
